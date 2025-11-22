1f2757e404b60fe9a0533c40a5a9b51fed55b871
Skoshi <CiaranMaudsley@protonmail.com>
2025-11-22 02:26:44 +0000

    Fixed bug

--- minecraftServer/serverMC.py
+++ minecraftServer/serverMC.py
@@ -5,13 +5,16 @@ def main():
         f.write("True")
     p = subprocess.Popen(["bash start.sh"], shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, bufsize=1, universal_newlines=True)
     for line in p.stdout:
-        if "Server empty for 60 seconds, pausing" in line:
+        if "<" or ">" in line:
+            print(line, end='')
+        elif "Server empty for 60 seconds, pausing" in line:
             break
         else:
             print(line, end='')
     p.communicate(input="/stop")
     with open("status.txt", "w") as f:
         f.write("False")
+
 
 
 
